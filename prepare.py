18ed51ae5dc1119face6fe12835a447ed1cb8b9e
rakshith <rakshith@umail.ucsb.edu>
2019-10-29 18:42:58 -0700

    Construct secret shares

--- prepare.py
+++ prepare.py
@@ -22,10 +22,23 @@ class Share():
         terms = ["{}*{}".format(c, x) for c,x in zip(coeff, variable_powers)]
         polynomial = "+".join(terms)
         self.polynomial = sympy.polys.polytools.poly(polynomial)
+
+    def secret_share(self):
+        self.populate_distinct_rand_share_keys()
+        shares = [(i,self.polynomial.eval(i)) for i in self.random_share_keys]
+        self.shares = shares
+        print(self.shares)
+    
+    def populate_distinct_rand_share_keys(self):
+        s = set()
+        while(len(s)<self.n):
+            s.add(random.randint(1,1000))
+        self.random_share_keys = s
     
 
 
 if __name__ == "__main__":
     share = Share(124, 6, 3)
     share.prepare_polynomial()
+    share.secret_share()
         
